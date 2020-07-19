fdd9abf5ce1731f7cf902f85bd5567c651487254
Yishuen Lo <lo.yishuen@gmail.com>
2020-07-19 09:34:50 -0600

    update images

--- src/scenes/components/Ingredient/Ingredient.js
+++ src/scenes/components/Ingredient/Ingredient.js
@@ -4,43 +4,11 @@ import classes from "./Ingredient.module.css";
 
 //rendering ingredients in Burger using CSS
 const Ingredient = ({ type }) => {
-  let style = null;
-
-  //assign style based on type
-  switch (type) {
-    case "bread-top":
-      style = classes.BreadTop;
-      break;
-    case "bread-bottom":
-      style = classes.BreadBottom;
-      break;
-    case "lettuce":
-      style = classes.Lettuce;
-      break;
-    case "onion":
-      style = classes.Onion;
-      break;
-    case "pickle":
-      style = classes.Pickle;
-      break;
-    case "tomato":
-      style = classes.Tomato;
-      break;
-    case "egg":
-      style = classes.Egg;
-      break;
-    case "bacon":
-      style = classes.Bacon;
-      break;
-    case "cheese":
-      style = classes.Cheese;
-      break;
-    case "protein":
-      style = classes.Protein;
-      break;
-  }
-
-  return <div className={style}></div>;
+  return (
+    <img
+      src={`https://raw.githubusercontent.com/yishuenlo/burger-bulider/design/src/assets/burger-ingredients/${type}.svg`}
+    />
+  );
 };
 
 Ingredient.prototype = {
